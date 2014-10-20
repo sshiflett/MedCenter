674687df1f1d0c6753bdea7d8a1748af13a75647
sshiflett <sshiflett@live.com>
2014-10-20 18:12:48 -0400

    More Updates

---
+++ src/awesomeapp/com/medcenter/DisplayPatientInfo.java
@@ -0,0 +1,18 @@
+package awesomeapp.com.medcenter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DisplayPatientInfo extends Activity {
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_patient_info);
+		
+	}
+	
+}
