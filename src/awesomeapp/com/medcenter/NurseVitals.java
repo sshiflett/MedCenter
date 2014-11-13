8d27f295a76128807e620d47f8c504ed289d84fa
sshiflett <sshiflett@live.com>
2014-11-13 13:32:32 -0500

    BH complete

--- src/awesomeapp/com/medcenter/NurseVitals.java
+++ src/awesomeapp/com/medcenter/NurseVitals.java
@@ -1,7 +1,8 @@
 package awesomeapp.com.medcenter;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Set;
-
 import zephyr.android.BioHarnessBT.BTClient;
 import zephyr.android.BioHarnessBT.BTReceiver;
 import zephyr.android.BioHarnessBT.ZephyrProtocol;
@@ -14,6 +15,8 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
@@ -28,7 +31,6 @@ public class NurseVitals extends Activity {
 	BTClient _bt;
 	ZephyrProtocol _protocol;
 	NewConnectedListener _NConnListener;
-	final Handler Newhandler = new Handler();
 	private final int HEART_RATE = 0x100;
 	private final int RESPIRATION_RATE = 0x101;
 	private final int SKIN_TEMPERATURE = 0x102;
@@ -39,9 +41,9 @@ public class NurseVitals extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_patient_vitals_final);
 		IntentFilter filter = new IntentFilter("android.bluetooth.device.action.PAIRING_REQUEST");
-		this.getApplicationContext().registerReceiver(new BTReceiver(), filter);
+		this.getApplicationContext().registerReceiver(new BTBroadcastReceiver(), filter);
 		IntentFilter filter2 = new IntentFilter("android.bluetooth.device.action.BOND_STATE_CHANGED");
-		this.getApplicationContext().registerReceiver(new BTReceiver(), filter2);
+		this.getApplicationContext().registerReceiver(new BTBondReceiver(), filter2);
 		
 		
 		Button next = (Button) findViewById(R.id.b_dh_logout);
@@ -103,15 +105,86 @@ public class NurseVitals extends Activity {
 		}
 	}
 
-	/*
-private class BTBondReceiver extends BroadcastReceiver{
-	@Override
-	public void onReceiver(Context context, Intent intent)
-	{
+
+private class BTBondReceiver extends BroadcastReceiver 
+{
+		@Override
+		public void onReceive(Context context, Intent intent) 
+		{
 			Bundle b = intent.getExtras();
 			BluetoothDevice device = adapter.getRemoteDevice(b.get("android.bluetooth.device.extra.DEVICE").toString());
-	}
-	
+			Log.d("Bond state", "BOND_STATED = " + device.getBondState());
+		}
 }
-*/
+
+private class BTBroadcastReceiver extends BroadcastReceiver 
+{
+	@Override
+	public void onReceive(Context context, Intent intent) 
+	{
+		Log.d("BTIntent", intent.getAction());
+		Bundle b = intent.getExtras();
+		Log.d("BTIntent", b.get("android.bluetooth.device.extra.DEVICE").toString());
+		Log.d("BTIntent", b.get("android.bluetooth.device.extra.PAIRING_VARIANT").toString());
+		try {
+			BluetoothDevice device = adapter.getRemoteDevice(b.get("android.bluetooth.device.extra.DEVICE").toString());
+			Method m = BluetoothDevice.class.getMethod("convertPinToBytes", new Class[] {String.class} );
+			byte[] pin = (byte[])m.invoke(device, "1234");
+			m = device.getClass().getMethod("setPin", new Class [] {pin.getClass()});
+			Object result = m.invoke(device, pin);
+			Log.d("BTTest", result.toString());
+		} catch (SecurityException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (NoSuchMethodException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
+final  Handler Newhandler = new Handler(){
+	public void handleMessage(Message msg)
+	{
+		TextView tv;
+		switch (msg.what)
+		{
+		case HEART_RATE:
+			String HeartRatetext = msg.getData().getString("HeartRate");
+			tv = (EditText)findViewById(R.id.et_heartRate);
+			System.out.println("Heart Rate Info is "+ HeartRatetext);
+			if (tv != null)tv.setText(HeartRatetext);
+		break;
+		
+		case RESPIRATION_RATE:
+			String RespirationRatetext = msg.getData().getString("RespirationRate");
+			tv = (EditText)findViewById(R.id.et_respirationRate);
+			if (tv != null)tv.setText(RespirationRatetext);
+		
+		break;
+		
+		case SKIN_TEMPERATURE:
+			String SkinTemperaturetext = msg.getData().getString("SkinTemperature");
+			tv = (EditText)findViewById(R.id.et_temperature);
+			if (tv != null)tv.setText(SkinTemperaturetext);
+
+		break;
+		
+		
+		
+		}
+	}
+
+};
+
+
 }
