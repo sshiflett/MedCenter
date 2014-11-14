e57322a133e90c30229c09fc08b313724d1b5e01
justinwetzel <justinwetzel@mail.usf.edu>
2014-11-14 15:03:31 -0500

    Using JSONTokener.

--- src/awesomeapp/com/medcenter/MainActivity.java
+++ src/awesomeapp/com/medcenter/MainActivity.java
@@ -17,6 +17,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -27,52 +28,31 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;    
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 
 public class MainActivity extends Activity {
 	
-	public String readJSONFeed(String URL) {
+	public String readJSONFeed(String URL)throws URISyntaxException, ClientProtocolException, IOException {
 
-		StringBuilder stringBuilder = new StringBuilder();
-
-		HttpClient client = new DefaultHttpClient();
-
-		HttpGet httpGet = new HttpGet(URL);
-		
-		JSONObject jObj = null;
-
-		try {
-
-			HttpResponse response = client.execute(httpGet);
-
-			HttpEntity entity = response.getEntity();
-
-			InputStream content = entity.getContent();
-
-			BufferedReader reader = new BufferedReader(
-
-			new InputStreamReader(content));
-
-			String line;
-
-			while ((line = reader.readLine()) != null) {
-
-				stringBuilder.append(line);
-
-			}
-
-		} catch (ClientProtocolException e) {
-
-			e.printStackTrace();
-
-		} catch (IOException e) {
-
-			e.printStackTrace();
-
-		}
-		
-		
-		 return stringBuilder.toString();
+		   HttpClient client = new DefaultHttpClient();
+           HttpGet request = new HttpGet();
+  
+           request.setURI(new URI(URL));
+           HttpResponse response = client.execute(request);
+          
+           
+           BufferedReader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+           StringBuffer sb = new StringBuffer("");
+           String line = "";
+           String NL = System.getProperty("line.separator");
+           while ((line = in.readLine()) != null) 
+           {
+               sb.append(line + NL);
+           }
+           in.close();
+     
+           return sb.toString();
 	}
 	
 
@@ -84,9 +64,9 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
-		
 		final EditText username = (EditText) findViewById(R.id.et_user_name);
 		final EditText password = (EditText) findViewById(R.id.et_input2);
+
 	
 		Button next = (Button) findViewById(R.id.b_ps_confirm);
 		next.setOnClickListener(new View.OnClickListener() {
@@ -94,41 +74,44 @@ public class MainActivity extends Activity {
 				@Override
 				public void onClick(View v) {
 		        	// Get entered text from EditText boxes.
+
+					
 		           	String userName = username.getText().toString();
 		        	String passWord = password.getText().toString();
-		        	int role_id = 0;
-		        	int userId = 0;
 		        	
+		        	JSONObject loginObject;
+		        	JSONTokener theTokener;
 		        	
+		        try{
 		            String httpRequest = "http://173.78.61.249:8080/api/authenticate/?username=" + userName + "&password=" + passWord;
 		            String httpResponse = readJSONFeed(httpRequest);
-		            
+		            theTokener = new JSONTokener(httpResponse);
 		            
 		        	
 		        	// Use "authenticate" function, use response.
-		             try{
-		             JSONObject loginObject = new JSONObject(httpResponse);
+		             
+		             loginObject = (JSONObject)theTokener.nextValue();
 		             int status = loginObject.getInt("status");  
 		        	// If authenticate status returns "forbidden", show error message using toast.
 		        	 if(status == 403)
 		        	 {
 		                	Toast.makeText(MainActivity.this, 
 		                	    "Your login was incorrect. Please try again.", Toast.LENGTH_SHORT).show();
-		               }     
+		             }     
 		        	
 		        	 else if(status == 202)
 		        	 	{
 		        	 		JSONObject userObject = loginObject.getJSONObject("user");
-		        	 		userId = userObject.getInt("id");
+		        	 		int userId = userObject.getInt("id");
 		        	 		// Send a request for user info using id returned from authenticate
-		             		String userLookup = "http://173.78.61.249:8080/api/user/?user_id="+ userId;
+		             		String userLookup = "http://173.78.61.249:8080/api/user/?user_id="+userId;
 		             		String userResponse = readJSONFeed(userLookup);
 		       
 		             		
-		             		try{
+		             		
 		                  		JSONObject user = new JSONObject(userResponse);
 		                  		JSONObject userInfo = user.getJSONObject("user");
-		             			role_id = userInfo.getInt("role");
+		             			int role_id = userInfo.getInt("role");
 		        				Intent shiftToDoctorHome = new Intent (v.getContext(), DoctorHome.class);
 		        				Intent shiftToPharm = new Intent (v.getContext(), PharmacistMainFinal.class);
 		        				Intent shiftToNurseHome = new Intent (v.getContext(), NurseHome.class);
@@ -147,15 +130,24 @@ public class MainActivity extends Activity {
 		        					startActivityForResult(shiftToPatientHome, 0);
 		        				}
 		             		}
-		             		catch(JSONException f){
 		             			//oops
-		             		}
-		        		}
-		             }
-		             catch(JSONException e)
-		             {
+		             	}
+		        		
+		             
+		       catch(JSONException e)
+		        {
 		            	 //oops
-		             }
+		        }
+		        
+		        catch(URISyntaxException ex)
+		        {
+		        	
+		        }
+		        catch(IOException ex)
+		        {
+		        	
+		        	
+		        }
 					
 				
 				}});
