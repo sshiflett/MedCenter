57329ec5584f65ad1f25d6676d6d092c0dd1fea3
sshiflett <sshiflett@mail.usf.edu>
2014-11-18 11:47:49 -0500

    Some touchups

--- src/awesomeapp/com/medcenter/Add_note.java
+++ src/awesomeapp/com/medcenter/Add_note.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 public class Add_note extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_add_note);
 	}
 
